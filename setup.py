6e08478cc7f5d52be44f1cb612527ad73fe55227
adamvis <Adam.viscusi@gmail.com>
2023-03-05 15:43:22 +0100

    Relaxes requirements

--- setup.py
+++ setup.py
@@ -22,10 +22,10 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/adamvis/pygraphprofiler",
     install_requires=[
-        'networkx==3.0',
-        'pandas==1.4.0',
-        'pygraphviz==1.10',
-        'matplotlib==3.5.1'
+        'networkx>=3.0',
+        'pandas>=1.4.0',
+        'pygraphviz>=1.10',
+        'matplotlib>=3.5.1'
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
